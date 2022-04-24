70b80acbc4bf2d69e9a827e816a7b5764ca462f7
Kaja Schmidt <kaja.schmidt@protonmail.com>
2022-04-24 13:27:49 +0200

    init necessary .js and .css files

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h2 className="header--title">Funkschein App</h2>
+            <h4 className="header--project">UBI, SRC und LRC</h4>
+        </header>
+    )
+}
---
+++ src/config.py
@@ -0,0 +1,2 @@
+RAW_DATA = "data/funkschein-questions-2018.xlsx"
+JSON_DATA = "data/src-lrc-ubi-fragekatalog-2018.json"
